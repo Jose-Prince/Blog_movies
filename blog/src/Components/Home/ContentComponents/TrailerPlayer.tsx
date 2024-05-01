c5d1259ff24829d9c16219c3c54ae2f1ebd253c1
Jose-Prince <joseprince148@gmail.com>
2024-04-30 18:41:21 -0600

    trailer responsive

--- blog/src/Components/Home/ContentComponents/TrailerPlayer.tsx
+++ blog/src/Components/Home/ContentComponents/TrailerPlayer.tsx
@@ -9,18 +9,23 @@ const TrailerPlayer = ({url}) => {
 
   return (
     <iframe 
-    style={{position: "absolute"}}
-      width="910" 
-      height="510" 
       src={url}
       title="YouTube video player" 
       frameBorder="0" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       referrerPolicy="strict-origin-when-cross-origin" 
       allowFullScreen
+      style={{
+        maxHeight: '508px',
+        height: 'calc(100vw / 1.78)',
+        maxWidth: '910px',
+        width: '100%',
+        marginRight: '10px',
+        marginLeft: '10px'
+      }}
     ></iframe>
-  );
-};
+  )
+}
 
 
 export default TrailerPlayer
