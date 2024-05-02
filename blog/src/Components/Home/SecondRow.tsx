42660b51ba7e618c401116ac385a3a518c98c0e6
Jose-Prince <joseprince148@gmail.com>
2024-05-02 15:45:51 -0600

    Home Screen Finished

--- blog/src/Components/Home/SecondRow.tsx
+++ blog/src/Components/Home/SecondRow.tsx
@@ -1,24 +1,122 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ComponentH.css'
 import Seat from '../../assets/Seat.svg'
+import { useDoubleClick } from '../../Hooks/useDoubleCLick'
+import useAPI from '../../Hooks/useAPI'
 
 interface ContainerProps {
-    setCountdownState : (countdownState : boolean) => void
+    setCountdownState: (countdownState : boolean) => void
+    setReplay: (countdownState : boolean) => void
+    index: number | null
+    setIndex: (index: number) => void
+    setScreenContent: (screenContent : boolean) => void
+    setPlayVideo: (playVideo: boolean) => void
 }
-const SecondRow: React.FC<ContainerProps> = ({setCountdownState}) => {
+const SecondRow: React.FC<ContainerProps> = ({
+    index, 
+    setIndex, 
+    setReplay,
+    setCountdownState, 
+    setScreenContent,
+    setPlayVideo,
+}) => {
+
+    const [startLoading, setStartLoading] = useState(false)
+    const [mousePosition, setMousePosition] = useState({
+        x: 0,
+        y: 0
+    })
+    const [timeoutId, setTimeoutId] = useState<number | null>(null)
+    const [, setCursor] = useState(false)
+    
+    const {movies} = useAPI()
+    
+    useEffect(() => {
+        const mouseMove = (e: { clientX: any; clientY: any }) => {
+            setMousePosition({
+                x: e.clientX,
+                y: e.clientY
+            })
+        }
+        window.addEventListener("mousemove", mouseMove)
+        
+        return () => {
+            window.removeEventListener("mousemove", mouseMove)
+        }
+    }, [])
+    
+    
+    const handleMouseLeave = () => {
+        setStartLoading(false)
+        if (timeoutId) {
+            window.clearTimeout(timeoutId)
+            setTimeoutId(null)
+        }
+    }
+
+    const handleOneClick = () => {
+        setScreenContent(false)
+        setPlayVideo(true)
+    }
+    
+    const hybridClick = useDoubleClick(
+        undefined,
+        handleOneClick,
+        undefined,
+        startLoading
+    )
 
     const imageSource: string = Seat
 
     const imageElements: JSX.Element[] = []
 
-    let leftp  = -237
-    let topp = 146
+    let leftp  = -3
+    let topp = 511
 
     for (let i = 0; i < 32; i++) {
         if (i % 8 == 0 && i != 0) {
-            topp += 85.7
-            leftp = -237
+            topp += 90
+            leftp = -3
+
         }
+
+        const handleCursor = () => {
+            if (i != index && i < movies.length){
+                return 'none'
+            } else if (i >= movies.length) {
+                return 'auto'
+            } else {
+                return 'pointer'
+            }
+        }
+    
+
+        const handleMouseEnter = () => {
+            if (i != index && i < movies.length) {
+                setCursor(false)
+                setStartLoading(true)
+                setReplay(true)
+                if (!timeoutId) {
+                    const id = window.setTimeout(() => {
+                        setCountdownState(false)
+                        setStartLoading(false)
+                        setReplay(false)
+                        setTimeoutId(null)
+                        console.log(i)
+                        setIndex(i)
+                        setCursor(true)
+                        setTimeout(() => {
+                            setCountdownState(true)
+                        }, 500);
+                    }, 3000)
+                    setTimeoutId(id)
+                }
+            } else {
+                setCursor(true)
+            }
+
+        }
+
 
         imageElements.push(
             <div key={i}>
@@ -26,30 +124,44 @@ const SecondRow: React.FC<ContainerProps> = ({setCountdownState}) => {
                     src={imageSource}
                     style={{ 
                         position: 'fixed',
+                        zIndex: '0',
                         top: topp +'px',
                         left: leftp + 'px',
-                        width: '716px', 
-                        height: '720px',
+                        width: '246px', 
                         margin: '0 0px', // Reducir el espacio entre las imágenes
                     }} 
                 />
-                <div 
+                <button 
                 style={{
                     position: 'fixed',
                     height: '176px',
-                    width: '234px',
-                    top: topp + 373+'px',
-                    left: leftp + 241 +'px',
-                    cursor: 'pointer',
-                    zIndex: '1'
-                }}></div>
-                
+                    width: '239px',
+                    top: topp +'px',
+                    left: leftp+3+'px',
+                    cursor: handleCursor(),
+                    zIndex: '1',
+                    backgroundColor: 'transparent',
+                    borderColor: 'transparent'
+                }}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onClick={hybridClick}
+                >
+                </button>
             </div>
         )
         leftp += 240
     }
 
-    return <>{imageElements}</>
+    return <>
+    {startLoading && <div 
+        className="loader" 
+        style={{
+            position: 'fixed', 
+            top: mousePosition.y -15,
+            left: mousePosition.x-18,    
+        }}/>}
+    {imageElements}</>
 }
 
 export default SecondRow
--- blog/src/pages/Home.tsx
+++ blog/src/pages/Home.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FirstRow from '../Components/Home/FirstRow'
 import Login from '../Components/Home/Login'
 import SecondRow from '../Components/Home/SecondRow'
 import './Home.css'
 
 const home: React.FC = () => {
+    const [isEditOpen, setIsEditOpen] = useState(false)
+    const [countdownState, setCountdownState] = useState(false)
+    const [replay, setReplay] = useState(false)
+    const [index, setIndex] = useState<number>(34)
+    const [screenContent, setScreenContent] = useState(false)
+    const [playVideo, setPlayVideo] = useState(false)
+
     return (
         <div className='home'>
             <Login />
-            <FirstRow />
-            <SecondRow />
+            <FirstRow 
+                index={index} 
+                replay={replay} 
+                countdownState={countdownState}
+                screenContent={screenContent}
+                setScreenContent={setScreenContent}
+                playVideo={playVideo}
+                setPlayVideo={setPlayVideo}
+            />
+            <SecondRow 
+                index={index} 
+                setIsEditOpen={setIsEditOpen} 
+                setIndex={setIndex} 
+                setReplay={setReplay} 
+                setCountdownState={setCountdownState}
+                setScreenContent={setScreenContent}
+                setPlayVideo={setPlayVideo}
+            />
         </div>
     )
 }
